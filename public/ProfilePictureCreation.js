03be97b2e5e2b7329f11115b24487ef3d23c1e5e
Hekkow <43420295+Hekkow@users.noreply.github.com>
2024-06-09 15:35:30 -0400

    added shapes and z indexing

--- public/ProfilePictureCreation.js
+++ public/ProfilePictureCreation.js
@@ -5,20 +5,26 @@ let ctx = canvas.getContext("2d")
 $('#profilePicCreatorBackground').css('display', 'flex') // remove
 let moving = false
 function drawShape() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-    ctx.save()
-    for (let [id, shape] of shapes.entries()) {
-        console.log(shape)
-        ctx.fillStyle = shape.color
-        ctx.translate(shape.rotationTranslationX, shape.rotationTranslationY)
-        ctx.rotate(shape.rotation)
-        ctx.translate(-shape.rotationTranslationX, -shape.rotationTranslationY)
-        ctx.fillRect(shape.x, shape.y, shape.w, shape.h)
-    }
-    ctx.restore()
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight)
+    let sortedShapes = Array.from(shapes.values()).sort((a, b) => a.z - b.z);
+    console.log("NEW")
+    $($('#shapesList').children().get().reverse()).each(function() {
+        console.log(this)
+        console.log($(this).attr('shapeID'))
+        let shape = shapes.get(parseInt($(this).attr('shapeID')))
+        ctx.save();
+        ctx.fillStyle = shape.color;
+        ctx.translate(shape.rotationTranslationX, shape.rotationTranslationY);
+        ctx.rotate(shape.rotation);
+        ctx.translate(-shape.rotationTranslationX, -shape.rotationTranslationY);
+        ctx.fillRect(shape.x, shape.y, shape.w, shape.h);
+        ctx.restore();
+    })
+
 }
 class Shape {
-    constructor(shapeID, x = canvasWidth/2-50/2, y = canvasHeight/2-50/2, w = 50, h = 50, rotation = 0, color = "#FF0000") {
+    constructor(shapeID, x = canvasWidth/2-50/2, y = canvasHeight/2-50/2, w = 50, h = 50, rotation = 0, color = "#FF0000", z = 0) {
         this.shapeID = shapeID
         this.x = x
         this.y = y
@@ -28,6 +34,7 @@ class Shape {
         this.updateRotationTranslationX()
         this.updateRotationTranslationY()
         this.color = color
+        this.z = z
     }
     addRotation(rotation) {
         this.rotation += rad(parseFloat(rotation))
@@ -56,6 +63,9 @@ class Shape {
     setColor(color) {
         this.color = color
     }
+    setZ(z) {
+        this.z = parseInt(z)
+    }
     updateRotationTranslationX() {
         this.rotationTranslationX = this.x + this.w/2
     }
@@ -66,16 +76,7 @@ class Shape {
 
 let shapes = new Map()
 let latestShapeID = 1
-let moveRange = 5
-let moveStep = 0.1
-let sizeRange = 0.05
-let sizeStep = 0.0001
-let rotationRange = 0.1
-let rotationStep = 0.001
-function continuousInput(shapeID, name, range, step) {
-    return `<label for="${name}${shapeID}">${name}</label>
-    <input type="range" name="${name}${shapeID}" class="${name}Slider continuousSlider" value=${0} min=${-range} max=${range} step=${step}>`
-}
+
 function createShape() {
     latestShapeID++
     let shapeID = latestShapeID
@@ -84,18 +85,21 @@ function createShape() {
 
     $('#shapesList').append(`
     <div class="shapeDiv" shapeID=${shapeID}>
+    <button onclick="up(${shapeID})">^</button>
+    <button onclick="down(${shapeID})">v</button>
         ${continuousInput(shapeID, 'x', moveRange, moveStep)}
         ${continuousInput(shapeID, 'y', moveRange, moveStep)}
         ${continuousInput(shapeID, 'Width', sizeRange, sizeStep)}
         ${continuousInput(shapeID, 'Height', sizeRange, sizeStep)}
         ${continuousInput(shapeID, 'Rotation', rotationRange, rotationStep)}
-        <label for="color${shapeID}">color</label>
-        <input type="color" name="color${shapeID}" class="colorSlider slider" value="${shape.color}">
+        ${createLabel(shapeID, "Color")}
+        <input type="color" name="color${shapeID}" class="colorSlider pfpInput" value="${shape.color}"></div>
+        ${createLabel(shapeID, "z-index")}
+        <input type="number" name="z-index${shapeID}" class="zSlider pfpInput" value=0></div>
     </div>
     `)
-    console.log(shape.color)
     drawShape()
-    $('.xSlider').on('mousedown', function (event) { // could possibly change this to get just added shape?
+    $('.xSlider').on('mousedown', function (event) { // could possibly change this to get just added slider instead of class
         startMoving(event, shapes.get(getShapeID(event)).addX)
     })
     $('.ySlider').on('mousedown', function (event) {
@@ -112,11 +116,26 @@ function createShape() {
     })
     $('.colorSlider').on('input', function (event) {
         shapes.get(getShapeID(event)).setColor(event.target.value)
+        drawShape()
+    })
+    $('.zSlider').on('input', function(event) {
+        shapes.get(getShapeID(event)).setZ(event.target.value)
+        drawShape()
     })
     $('.continuousSlider').on('change', e => {
         moving = false
         $(e.target).val(0)
     })
+}
+function up(shapeID) {
+    let shape = $(`.shapeDiv[shapeID=${shapeID}]`)
+    shape.insertBefore(shape.prev())
+    drawShape()
+}
+function down(shapeID) {
+    let shape = $(`.shapeDiv[shapeID=${shapeID}]`)
+    shape.insertAfter(shape.next())
+    drawShape()
 }
 function startMoving(event, f) {
     moving = true
@@ -128,8 +147,23 @@ function startMoving(event, f) {
         requestAnimationFrame(animate)
     })
 }
+
+let moveRange = 5
+let moveStep = 0.1
+let sizeRange = 0.1
+let sizeStep = 0.0001
+let rotationRange = 0.1
+let rotationStep = 0.001
+function createLabel(shapeID, name) {
+    return `<div class="sliderRow"><label for="${name}${shapeID}">${name}</label>`
+}
+function continuousInput(shapeID, name, range, step) {
+    return `${createLabel(shapeID, name)}
+    <input type="range" name="${name}${shapeID}" class="${name}Slider continuousSlider pfpInput" value=${0} min=${-range} max=${range} step=${step}></div>`
+}
+
 function getShapeID(event) {
-    return parseInt($(event.target).parent().attr('shapeID'))
+    return parseInt($(event.target).closest(".shapeDiv").attr('shapeID'))
 }
 function rad(deg) {
     return deg * Math.PI/180
