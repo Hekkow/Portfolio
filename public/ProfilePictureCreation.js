5b830ab14c28da4a28e07c13c674d49cdee82458
Hekkow <43420295+Hekkow@users.noreply.github.com>
2024-06-10 15:16:33 -0400

    added circle

--- public/ProfilePictureCreation.js
+++ public/ProfilePictureCreation.js
@@ -7,98 +7,102 @@ let moving = false
 function drawShape() {
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvasWidth, canvasHeight)
-    let sortedShapes = Array.from(shapes.values()).sort((a, b) => a.z - b.z);
-    console.log("NEW")
+    // draws each shape starting from the last in the list
+    // so that the ones on top of the list show above the ones at the bottom
     $($('#shapesList').children().get().reverse()).each(function() {
-        console.log(this)
-        console.log($(this).attr('shapeID'))
         let shape = shapes.get(parseInt($(this).attr('shapeID')))
         ctx.save();
-        ctx.fillStyle = shape.color;
-        ctx.translate(shape.rotationTranslationX, shape.rotationTranslationY);
-        ctx.rotate(shape.rotation);
-        ctx.translate(-shape.rotationTranslationX, -shape.rotationTranslationY);
-        ctx.fillRect(shape.x, shape.y, shape.w, shape.h);
-        ctx.restore();
+        ctx.fillStyle = shape.color
+        ctx.translate(shape.rotationTranslationX, shape.rotationTranslationY)
+        ctx.rotate(shape.rotation)
+        ctx.translate(-shape.rotationTranslationX, -shape.rotationTranslationY)
+        switch (shape.shape) {
+            case Shapes.Rectangle:
+                ctx.fillRect(shape.x, shape.y, shape.w, shape.h)
+                break
+            case Shapes.Circle:
+                ctx.beginPath()
+                // + shape.r to center it around x, y
+                ctx.arc(shape.x+shape.r, shape.y+shape.r, shape.r, 0, Math.PI * 2)
+                ctx.closePath()
+                ctx.fill()
+
+                break
+            case Shapes.Polygon:
+                let lines = [{ angle: 60, length: 100 }, { angle: 180, length: 100 }, { angle: -60, length: 100 }]
+                ctx.beginPath()
+                let previous = {x: 150, y: 100}
+                ctx.moveTo(previous.x, previous.y)
+                for (let line of lines) {
+                    previous = {x: previous.x+Math.cos(rad(line.angle)) * line.length, y: previous.y+Math.sin(rad(line.angle)) * line.length}
+                    console.log(previous)
+                    ctx.lineTo(previous.x, previous.y)
+                }
+
+                ctx.closePath()
+                ctx.fill()
+                break
+        }
+        ctx.restore()
     })
-
 }
-class Shape {
-    constructor(shapeID, x = canvasWidth/2-50/2, y = canvasHeight/2-50/2, w = 50, h = 50, rotation = 0, color = "#FF0000", z = 0) {
-        this.shapeID = shapeID
-        this.x = x
-        this.y = y
-        this.w = w
-        this.h = h
-        this.rotation = rotation
-        this.updateRotationTranslationX()
-        this.updateRotationTranslationY()
-        this.color = color
-        this.z = z
-    }
-    addRotation(rotation) {
-        this.rotation += rad(parseFloat(rotation))
-    }
-    addX(x) {
-        this.x += parseFloat(x)
-        this.updateRotationTranslationX()
-    }
-    addY(y) {
-        this.y += parseFloat(y)
-        this.updateRotationTranslationY()
-    }
-    addW(w) {
-        let oldCenterX = this.rotationTranslationX
-        this.w += parseFloat(w)
-        this.x = oldCenterX - this.w / 2
-        this.updateRotationTranslationX()
-    }
 
-    addH(h) {
-        let oldCenterY = this.rotationTranslationY
-        this.h += parseFloat(h)
-        this.y = oldCenterY - this.h / 2
-        this.updateRotationTranslationY()
-    }
-    setColor(color) {
-        this.color = color
-    }
-    setZ(z) {
-        this.z = parseInt(z)
-    }
-    updateRotationTranslationX() {
-        this.rotationTranslationX = this.x + this.w/2
-    }
-    updateRotationTranslationY() {
-        this.rotationTranslationY = this.y + this.h/2
-    }
+const Shapes = {
+    Rectangle: 0,
+    Circle: 1,
+    Polygon: 2,
 }
+
+
 
 let shapes = new Map()
-let latestShapeID = 1
-
+let latestShapeID = 2
+function shapesDropdown(shapeID) {
+    let select = `<select class="shapeSelect pfpInput" id="selectShape${shapeID}">`
+    for (let shape in Shapes) {
+        select += `<option value="${Shapes[shape]}">${shape}</option>`
+    }
+    return select + '</select>'
+}
 function createShape() {
-    latestShapeID++
-    let shapeID = latestShapeID
-    let shape = new Shape(shapeID)
+    let shape = new Rectangle()
+    console.log(shape)
+    let shapeID = shape.shapeID
     shapes.set(shapeID, shape)
+    showSliders(shapeID, shape)
+    drawShape()
 
-    $('#shapesList').append(`
+}
+function showSliders(shapeID, shape) {
+    let shapesList = $('#shapesList')
+    shapesList.find(`.shapeDiv[shapeID=${shapeID}]`).remove()
+    let div = `
     <div class="shapeDiv" shapeID=${shapeID}>
-    <button onclick="up(${shapeID})">^</button>
-    <button onclick="down(${shapeID})">v</button>
-        ${continuousInput(shapeID, 'x', moveRange, moveStep)}
-        ${continuousInput(shapeID, 'y', moveRange, moveStep)}
-        ${continuousInput(shapeID, 'Width', sizeRange, sizeStep)}
-        ${continuousInput(shapeID, 'Height', sizeRange, sizeStep)}
-        ${continuousInput(shapeID, 'Rotation', rotationRange, rotationStep)}
+        <button onclick="up(${shapeID})">^</button>
+        <button onclick="down(${shapeID})">v</button>
+        ${shapesDropdown(shapeID)}
+        ${continuousInput(shapeID, 'x', shape.moveRange, shape.moveStep)}
+        ${continuousInput(shapeID, 'y', shape.moveRange, shape.moveStep)}
         ${createLabel(shapeID, "Color")}
         <input type="color" name="color${shapeID}" class="colorSlider pfpInput" value="${shape.color}"></div>
-        ${createLabel(shapeID, "z-index")}
-        <input type="number" name="z-index${shapeID}" class="zSlider pfpInput" value=0></div>
-    </div>
-    `)
-    drawShape()
+    `
+    if ([Shapes.Rectangle].includes(shape.shape)) {
+        div += `
+        ${continuousInput(shapeID, 'Width', shape.sizeRange, shape.sizeStep)}
+        ${continuousInput(shapeID, 'Height', shape.sizeRange, shape.sizeStep)}`
+    }
+    if ([Shapes.Circle].includes(shape.shape)) {
+        div += `${continuousInput(shapeID, 'Radius', shape.radiusRange, shape.radiusStep)}`
+    }
+    if (![Shapes.Circle].includes(shape.shape)) {
+        div += `${continuousInput(shapeID, 'Rotation', shape.rotationRange, shape.rotationStep)}`
+    }
+    if ([Shapes.Polygon].includes(shape.shape)) {
+        div += `<button onclick="increaseSides(shapeID)">Increase sides</button>`
+    }
+    div += '</div>'
+    shapesList.append(div)
+    shapesList.find(`#selectShape${shapeID}`).val(shape.shape)
     $('.xSlider').on('mousedown', function (event) { // could possibly change this to get just added slider instead of class
         startMoving(event, shapes.get(getShapeID(event)).addX)
     })
@@ -111,6 +115,9 @@ function createShape() {
     $('.HeightSlider').on('input', function (event) {
         startMoving(event, shapes.get(getShapeID(event)).addH)
     })
+    $('.RadiusSlider').on('input', function (event) {
+        startMoving(event, shapes.get(getShapeID(event)).addR)
+    })
     $('.RotationSlider').on('input', function (event) {
         startMoving(event, shapes.get(getShapeID(event)).addRotation)
     })
@@ -118,15 +125,19 @@ function createShape() {
         shapes.get(getShapeID(event)).setColor(event.target.value)
         drawShape()
     })
-    $('.zSlider').on('input', function(event) {
-        shapes.get(getShapeID(event)).setZ(event.target.value)
+    $('.shapeSelect').on('change', function (event) {
+        let shapeID = getShapeID(event)
+        shapes.set(shapeID, shapeFactory(shape, event.target.value, shapeID))
         drawShape()
+        showSliders(shapeID, shapes.get(shapeID))
+
     })
     $('.continuousSlider').on('change', e => {
         moving = false
         $(e.target).val(0)
     })
 }
+
 function up(shapeID) {
     let shape = $(`.shapeDiv[shapeID=${shapeID}]`)
     shape.insertBefore(shape.prev())
@@ -148,12 +159,7 @@ function startMoving(event, f) {
     })
 }
 
-let moveRange = 5
-let moveStep = 0.1
-let sizeRange = 0.1
-let sizeStep = 0.0001
-let rotationRange = 0.1
-let rotationStep = 0.001
+
 function createLabel(shapeID, name) {
     return `<div class="sliderRow"><label for="${name}${shapeID}">${name}</label>`
 }
@@ -167,4 +173,105 @@ function getShapeID(event) {
 }
 function rad(deg) {
     return deg * Math.PI/180
+}
+class Shape {
+    constructor(shapeID = -1, x = 0, y = 0, color = "#FF0000") {
+        if (shapeID === -1) {
+            this.shapeID = latestShapeID
+            latestShapeID++
+        }
+        else this.shapeID = shapeID
+        this.x = x
+        this.y = y
+        this.color = color
+        this.rotation = 0
+        this.moveRange = 5
+        this.moveStep = 0.1
+        this.rotationRange = 0.1
+        this.rotationStep = 0.001
+    }
+    addRotation(rotation) {
+        this.rotation += rad(parseFloat(rotation))
+    }
+    addX(x) {
+        this.x += parseFloat(x)
+    }
+    addY(y) {
+        this.y += parseFloat(y)
+    }
+    setColor(color) {
+        this.color = color
+    }
+
+}
+class Rectangle extends Shape {
+    constructor(shapeID = -1, x = canvasWidth/2-50/2, y = canvasHeight/2-50/2, color = "#FF0000") {
+        super(shapeID, x, y, color)
+        this.shape = Shapes.Rectangle
+        this.sizeRange = 0.15
+        this.sizeStep = 0.0001
+        this.w = 50
+        this.h = 50
+        this.updateRotationTranslationX()
+        this.updateRotationTranslationY()
+    }
+    addX(x) {
+        super.addX(x)
+        this.updateRotationTranslationX()
+    }
+    addY(y) {
+        super.addY(y)
+        this.updateRotationTranslationY()
+    }
+    addW(w) {
+        let oldCenterX = this.rotationTranslationX
+        this.w += parseFloat(w)
+        this.x = oldCenterX - this.w / 2
+        this.updateRotationTranslationX()
+    }
+    addH(h) {
+        let oldCenterY = this.rotationTranslationY
+        this.h += parseFloat(h)
+        this.y = oldCenterY - this.h / 2
+        this.updateRotationTranslationY()
+    }
+    updateRotationTranslationX() {
+        this.rotationTranslationX = this.x + this.w/2
+    }
+    updateRotationTranslationY() {
+        this.rotationTranslationY = this.y + this.h/2
+    }
+}
+class Circle extends Shape {
+    constructor(shapeID = -1, x = 0, y = 0, color = "#FF0000") {
+        super(shapeID, x, y, color)
+        this.shape = Shapes.Circle
+        this.r = 25
+        this.radiusRange = 0.1
+        this.radiusStep = 0.0001
+    }
+    addR(r) {
+        let difference = this.r
+        this.r += parseFloat(r)
+        if (this.r <= 0) this.r = 0
+        difference -= this.r
+        this.x += difference
+        this.y += difference
+    }
+}
+class Polygon extends Shape {
+    constructor(shapeID = -1, x = 0, y = 0, color = "#FF0000") {
+        super(shapeID, x, y, color)
+        this.shape = Shapes.Polygon
+    }
+}
+function shapeFactory(shape, newShape, shapeID) {
+    switch (parseInt(newShape)) {
+        case Shapes.Rectangle:
+            return new Rectangle(shapeID, shape.x, shape.y, shape.color)
+        case Shapes.Circle:
+            return new Circle(shapeID, shape.x, shape.y, shape.color)
+        case Shapes.Polygon:
+            return new Polygon(shapeID, shape.x, shape.y, shape.color)
+    }
 }
